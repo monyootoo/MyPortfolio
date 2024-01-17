619ba85197977c07022b6559372e7f7c3a60059f
Moyartu Manley <141173463+monyootoo@users.noreply.github.com>
2024-01-16 23:21:44 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // get the target element's offsetTop and subtract a desired offset
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
-        const offset = 70;
+        const offset = 125;
         const targetOffsetTop = targetElement.offsetTop - offset;
 
         // scroll to the target element with the offset
@@ -20,28 +20,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  //intersection observer
-  const options = {
-    root: document.documentElement,//document.querySelector('.nav'), // The viewport element
-    threshold: 0.55, // Intersection trigger at 25% visibility
-  };
-  
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // Remove active class from previously active links
-        document.querySelectorAll('.links a.active').forEach(link => link.classList.remove('active'));
-  
-        // Get ID of the intersecting section
-        const sectionId = entry.target.id;
-  
-        // Find matching link in nav bar and add active class
-        const navLink = document.querySelector(`.links a[href="#${sectionId}"]`);
-        navLink.classList.add('active');
-      }
-    });
-  }, options);
-  
-  // Observe all sections
-  const sections = document.querySelectorAll('.section');
-  sections.forEach(section => observer.observe(section));
